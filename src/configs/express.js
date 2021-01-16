24ec9abdf0eed082e705ebbecf265c221328e8d7
Pattadon Prongpreecha <pattadon.pro@ascendcorp.com>
2021-01-17 03:30:04 +0700

    config cors

--- src/configs/express.js
+++ src/configs/express.js
@@ -5,7 +5,9 @@ const express = require('express'),
 module.exports = async (app) => {
     require('../configs/databases');
 
-    app.use(cors());
+    app.use(cors({
+        exposedHeaders: ['X-Total-Count'],
+    }));
 
     app.use(express.json());
     app.use(express.urlencoded({extended: false}));
